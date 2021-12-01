0817e416b375f014804f4e9abdc57b7b4bf21e94
Renato Fialho <482565+fialhorenato@users.noreply.github.com>
2021-12-02 00:35:12 +0100

    Improving readability

--- src/main/kotlin/task1/Main.kt
+++ src/main/kotlin/task1/Main.kt
@@ -2,28 +2,28 @@ package task1
 
 import util.Input
 
-fun main() {
-    val input = Input.readIntegers("day1.txt")
-    println(countIncrements(input))
-    println(countMovingIncrements(input))
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val arg = if (args.isNotEmpty()) args[0] else "day1.txt"
+            val input = Input.readIntegers(arg)
+            println(countIncrements(input))
+            println(countMovingIncrements(input))
+        }
+    }
 }
 
 private fun countIncrements(input : List<Int>): Int {
     return input
-            // Creates a window of 2 numbers
         .windowed(2, 1)
-            // Count if the first number is less than the second number
         .count{ (x,y) -> x < y}
 }
 
 private fun countMovingIncrements(input : List<Int>): Int {
-    val movingAverage =  input
-            // Creates windows of 3 numbers
+    return input
         .windowed(3,1)
-            // Sum those numbers
         .map { it.sum() }
-
-    // Call the previous function to count the increments
-    return countIncrements(movingAverage)
+        .let { countIncrements(it) }
 }
 
